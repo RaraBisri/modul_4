069799d1b3b7e5923c51a25cd185b5c04597d5c5
RaraBisri <rarabisri@gmail.com>
2022-11-08 19:52:17 +0800

    Create latihan2.java

---
+++ latihan2.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modul_4;
+
+/**
+ *
+ * @author USER
+ */
+public class latihan2 {
+  public static void main(String[] args) {
+        int n1 = 10;
+        int n2 = 20;
+        int n3 = 45;
+        int average;
+        
+        System.out.println("number 1 = " + n1);
+        System.out.println("number 2 = " + n2);
+        System.out.println("number 3 = " + n3);
+        
+        average = (n1 + n2 + n3)/3;
+        System.out.println("Average = " + average);
+    }  
+}
